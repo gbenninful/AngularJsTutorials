f386f88546b033ae5f7581bbc23c1fdebca7be65
gbenninful <enninful@hotmail.com>
2013-11-19 07:43:14 -0800

    separating scripts into script file

--- AngularJsTutorials/Script/AngularJsTutorial.js
+++ AngularJsTutorials/Script/AngularJsTutorial.js
@@ -1,1 +1,32 @@
-﻿
+﻿var demoApp = angular.module('demoApp', ['ngRoute']);
+
+demoApp.config(function ($routeProvider) {
+    $routeProvider
+    .when('/view1',
+        {
+            controller: 'SimpleController',
+            templateUrl: 'Views/View1.html'
+        })
+    .when('/view2',
+    {
+        controller: 'SimpleController',
+        templateUrl: 'Views/View2.html'
+    })
+    .otherwise({ redirectTo: '/view1' });
+})
+.controller('SimpleController', function ($scope) {
+    $scope.customers = [
+        { name: 'John Doe', city: 'New York' },
+        { name: 'John Smith', city: 'Phoenix' },
+        { name: 'Jane Doe', city: 'San Francisco' }
+    ];
+
+    $scope.addCustomer = function () {
+        $scope.customers.push(
+            {
+                name: $scope.newCustomer.name,
+                city: $scope.newCustomer.city
+            });
+    };
+});
+
